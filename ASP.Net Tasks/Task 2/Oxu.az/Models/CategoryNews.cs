e7c0e3b145081fd2247749d78f541e6dddcfff56
Nazaralieff <nazaralyeff@gmail.com>
2021-12-18 16:07:26 +0400

    CategoryNews Class Added

---
+++ ASP.Net Tasks/Task 2/Oxu.az/Models/CategoryNews.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Oxu.az.Models
+{
+    public class CategoryNews
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+
+        public News News { get; set; }
+
+
+        public Category Category { get; set; }
+
+    }
+}
