2b4176ec973e6ebdb822abc4c9bdebb17455ed02
Nazaralieff <nazaralyeff@gmail.com>
2021-12-18 21:06:37 +0400

    Add AuthorNews Class

---
+++ ASP.Net Tasks/Task 2/Oxu.az/Models/AuthorNews.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Oxu.az.Models
+{
+    public class AuthorNews
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        public Author Author { get; set; }
+        public News News { get; set; }
+    }
+}
