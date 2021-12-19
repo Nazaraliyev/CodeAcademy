6b79d69ad9f1e82abc863c6aefd29a9d7cbd98ec
Nazaralieff <nazaralyeff@gmail.com>
2021-12-19 17:16:50 +0400

    Getting Database data

--- ASP.Net Tasks/Task 3/SixteenClothing/Controllers/HomeController.cs
+++ ASP.Net Tasks/Task 3/SixteenClothing/Controllers/HomeController.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SixteenClothing.Data;
+using SixteenClothing.Models;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
 
 namespace SixteenClothing.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            this._context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Product> products = _context.Products.ToList();
+            List<HomeBanners> banner = _context.HomeBanners.ToList();
+            dynamic model = new ExpandoObject();
+            model.Banner = banner;
+            model.Product = products;
+            return View(model);
         }
     }
 }
