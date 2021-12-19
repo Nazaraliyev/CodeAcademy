369919debcd06ff257819f73924a1dc920b667ab
Nazaralieff <nazaralyeff@gmail.com>
2021-12-19 13:55:17 +0400

    Added Migration Folder and Files

---
+++ ASP.Net Tasks/Task 3/SixteenClothing/Migrations/20211219095456_init.cs
@@ -0,0 +1,248 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SixteenClothing.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "contactUsAccordions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Header = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Text = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_contactUsAccordions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HomeBanners",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SubHeader = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Header = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Img = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HomeBanners", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fullname = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Mail = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Subject = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Text = table.Column<string>(type: "ntext", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Partners",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Logo = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Partners", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Img = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Text = table.Column<string>(type: "ntext", nullable: true),
+                    Price = table.Column<int>(type: "int", nullable: false),
+                    Star = table.Column<byte>(type: "tinyint", nullable: false),
+                    Preview = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Qualities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Header = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Text = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Qualities", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Settings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Logo = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    HomeContent = table.Column<string>(type: "ntext", nullable: true),
+                    HomeContentImg = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    OurProductSubHeader = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    OurProductHeader = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    OurProductImg = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    AboutUsSubHeader = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    AboutUsHeader = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    AboutUsImg = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    AboutUsContent = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    AboutUsContentImg = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    ContactUsSubHeader = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    ContactUsHeader = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    ContactUsImg = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    ContactUsContent = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    ContactUsLocation = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Settings", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SosialMedias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Icon = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Link = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SosialMedias", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Specialities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Specialities", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TeamMembers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Surname = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Img = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Text = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    SpecialityId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TeamMembers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TeamMembers_Specialities_SpecialityId",
+                        column: x => x.SpecialityId,
+                        principalTable: "Specialities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MemberSosialNetworks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Icon = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    NLinkame = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    TeamMembersId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MemberSosialNetworks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MemberSosialNetworks_TeamMembers_TeamMembersId",
+                        column: x => x.TeamMembersId,
+                        principalTable: "TeamMembers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MemberSosialNetworks_TeamMembersId",
+                table: "MemberSosialNetworks",
+                column: "TeamMembersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TeamMembers_SpecialityId",
+                table: "TeamMembers",
+                column: "SpecialityId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "contactUsAccordions");
+
+            migrationBuilder.DropTable(
+                name: "HomeBanners");
+
+            migrationBuilder.DropTable(
+                name: "MemberSosialNetworks");
+
+            migrationBuilder.DropTable(
+                name: "Messages");
+
+            migrationBuilder.DropTable(
+                name: "Partners");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Qualities");
+
+            migrationBuilder.DropTable(
+                name: "Settings");
+
+            migrationBuilder.DropTable(
+                name: "SosialMedias");
+
+            migrationBuilder.DropTable(
+                name: "TeamMembers");
+
+            migrationBuilder.DropTable(
+                name: "Specialities");
+        }
+    }
+}
