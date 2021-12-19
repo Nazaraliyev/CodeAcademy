c5001c399b8ff888592a4bf68a8f5c9898a96a73
Nazaralieff <nazaralyeff@gmail.com>
2021-12-19 17:44:49 +0400

    Integrated Database to OurProducts Controller

--- ASP.Net Tasks/Task 3/SixteenClothing/Controllers/OurProductsController.cs
+++ ASP.Net Tasks/Task 3/SixteenClothing/Controllers/OurProductsController.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SixteenClothing.Data;
+using SixteenClothing.Models;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
 
 namespace SixteenClothing.Controllers
 {
     public class OurProductsController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public OurProductsController(AppDbContext context)
+        {
+            this._context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Product> product = _context.Products.ToList();
+            Settings settings =  _context.Settings.FirstOrDefault();
+
+            dynamic model = new ExpandoObject();
+
+            model.settings = settings;
+            model.products = product;
+            return View(model);
         }
     }
 }
