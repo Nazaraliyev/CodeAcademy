59f436635d70cd5d5defecd72bd55194f442960a
Nazaralieff <nazaralyeff@gmail.com>
2021-12-19 13:43:31 +0400

    Add AppDbContext Class

---
+++ ASP.Net Tasks/Task 3/SixteenClothing/Data/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SixteenClothing.Models;
+
+namespace SixteenClothing.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Settings> Settings { get; set; }
+
+        public DbSet<HomeBanners> HomeBanners { get; set; }
+
+        public DbSet<ContactUsAccordions> contactUsAccordions { get; set; }
+
+        public DbSet<SosialMedia> SosialMedias { get; set; }
+
+        public DbSet<Partners> Partners { get; set; }
+
+        public DbSet<Quality> Qualities { get; set; }
+
+        public DbSet<Speciality> Specialities { get; set; }
+
+        public DbSet<TeamMembers> TeamMembers { get; set; }
+
+        public DbSet<MemberSosialNetwork> MemberSosialNetworks { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Messages> Messages { get; set; }
+
+    }
+}
