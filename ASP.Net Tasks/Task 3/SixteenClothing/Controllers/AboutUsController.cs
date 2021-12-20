d1de74aa854cb9c44859544edba72db19a7aac96
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2021-12-21 00:26:21 +0400

    include Speciality to AboutUsController

--- ASP.Net Tasks/Task 3/SixteenClothing/Controllers/AboutUsController.cs
+++ ASP.Net Tasks/Task 3/SixteenClothing/Controllers/AboutUsController.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SixteenClothing.Data;
+using SixteenClothing.ViewModel;
+using System.Linq;
 
 namespace SixteenClothing.Controllers
 {
     public class AboutUsController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public AboutUsController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            VmAboutUs model = new VmAboutUs()
+            {
+                teams = _context.TeamMembers.Include("Speciality").ToList(),
+                qualities = _context.Qualities.ToList(),
+                settings = _context.Settings.FirstOrDefault()
+            };
+            return View(model);
         }
     }
 }
