92c8138543c00a29a45343a1987f86defd8728ca
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2021-12-21 00:26:54 +0400

    Has done Mirgations

--- ASP.Net Tasks/Task 3/SixteenClothing/Migrations/20211220190550_UpdateSetting.Designer.cs
+++ ASP.Net Tasks/Task 3/SixteenClothing/Migrations/20211220190550_UpdateSetting.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SixteenClothing.Data;
 
 namespace SixteenClothing.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211220190550_UpdateSetting")]
+    partial class UpdateSetting
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -222,7 +224,8 @@ namespace SixteenClothing.Migrations
                         .HasColumnType("nvarchar(50)");
 
                     b.Property<string>("ContactUsContent")
-                        .HasColumnType("ntext");
+                        .HasMaxLength(50)
+                        .HasColumnType("nvarchar(50)");
 
                     b.Property<string>("ContactUsHeader")
                         .HasMaxLength(50)
---
+++ ASP.Net Tasks/Task 3/SixteenClothing/Migrations/20211220190550_UpdateSetting.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SixteenClothing.Migrations
+{
+    public partial class UpdateSetting : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AboutUsContent",
+                table: "Settings",
+                type: "ntext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AboutUsContent",
+                table: "Settings",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "ntext",
+                oldNullable: true);
+        }
+    }
+}
