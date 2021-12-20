48f21252756cf69ce2328f0a0dfaffa7e0dc9fa7
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2021-12-21 00:27:21 +0400

    Has Done VmAbout

---
+++ ASP.Net Tasks/Task 3/SixteenClothing/ViewModel/VmAboutUs.cs
@@ -0,0 +1,14 @@
+﻿using SixteenClothing.Models;
+using System.Collections.Generic;
+
+namespace SixteenClothing.ViewModel
+{
+    public class VmAboutUs:VmSettings
+    {
+        public List<TeamMembers> teams { get; set; }
+
+        public List<Quality> qualities { get; set; }
+
+        public List<Partners> partners { get; set; }
+    }
+}
