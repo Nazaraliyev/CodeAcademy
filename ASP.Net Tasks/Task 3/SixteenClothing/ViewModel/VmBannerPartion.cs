25402cbfc98dd3bc5692722535502c49088a71fd
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2021-12-21 00:36:39 +0400

    Created VmBannerPartial

---
+++ ASP.Net Tasks/Task 3/SixteenClothing/ViewModel/VmBannerPartion.cs
@@ -0,0 +1,9 @@
+﻿namespace SixteenClothing.ViewModel
+{
+    public class VmBannerPartion
+    {
+        public string Img { get; set; }
+        public string SubHeader { get; set; }
+        public string Header { get; set; }
+    }
+}
