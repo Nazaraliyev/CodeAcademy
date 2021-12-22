93e672f7eb7cec4e2c907260012fb08a2f79b54a
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2021-12-23 03:27:33 +0400

    AppDbContext class has been ready

--- ASP.Net Tasks/Task 4/Repair/Data/AppDbContext.cs
+++ ASP.Net Tasks/Task 4/Repair/Data/AppDbContext.cs
@@ -1,6 +1,33 @@
-﻿namespace Repair.Data
+﻿using Microsoft.EntityFrameworkCore;
+using Repair.Models;
+
+namespace Repair.Data
 {
-    public class AppDbContext
+    public class AppDbContext:DbContext
     {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Settings> settings { get; set; }
+        public DbSet<SendedMessages> sendedMessages { get; set; }
+        public DbSet<WorksCount> worksCounts { get; set; }
+        public DbSet<SosialMedias> sosialMedias { get; set; }
+        public DbSet<Partners> partners { get; set; }
+        public DbSet<Subscribes> subscribes { get; set; }
+        public DbSet<EstimateList> estimateLists { get; set; }
+        public DbSet<Services> services { get; set; }
+        public DbSet<FeedBacks> feedBacks { get; set; }
+        public DbSet<Accordion> accordions { get; set; }
+        public DbSet<WorkingProsesStep> workingProses { get; set; }
+        public DbSet<Tag> tags { get; set; }
+        public DbSet<Author> authors { get; set; }
+        public DbSet<AuthorSosialMedias> authorSosialMedias { get; set; }
+        public DbSet<BlogCategory> blogCategories { get; set; }
+        public DbSet<Blog> blogs { get; set; }
+        public DbSet<tagToBlog> tagToBlogs { get; set; }
+        public DbSet<AuthorToBlog> authorToBlogs { get; set; }
+        public DbSet<Blogimage> blogimages { get; set; }
+        public DbSet<BlogComment> blogComments { get; set; }
     }
 }
