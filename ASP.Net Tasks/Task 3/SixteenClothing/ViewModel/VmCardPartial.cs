b4bc72269973bc3f10edaa65f56d7de8eef8e8ea
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2021-12-22 17:49:27 +0400

    Add VmCardPartial

---
+++ ASP.Net Tasks/Task 3/SixteenClothing/ViewModel/VmCardPartial.cs
@@ -0,0 +1,17 @@
+﻿namespace SixteenClothing.ViewModel
+{
+    public class VmCardPartial
+    {
+        public string Img { get; set; }
+
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+
+        public string Text { get; set; }
+
+        public int Preview { get; set; }
+
+
+    }
+}
