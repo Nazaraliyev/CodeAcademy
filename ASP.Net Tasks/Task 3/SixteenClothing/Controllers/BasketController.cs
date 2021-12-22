df192df034dbf0f85544ce415d03f667ccf202dd
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2021-12-23 01:17:52 +0400

    basketController has done

--- ASP.Net Tasks/Task 3/SixteenClothing/Controllers/BasketController.cs
+++ ASP.Net Tasks/Task 3/SixteenClothing/Controllers/BasketController.cs
@@ -20,25 +20,22 @@ namespace SixteenClothing.Controllers
             VmBasket model = new VmBasket()
             {
                 settings = _context.Settings.FirstOrDefault(),
-            };
-
+                products = new List<Product>()
+        };
             string basket = Request.Cookies["basket"];
             List<Product> products = _context.Products.ToList();
             if (!string.IsNullOrEmpty(basket))
             {
                 List<string> datalist = basket.Split("-").ToList();
-                products.ForEach(item =>
+                foreach(var item in products)
                 {
                     if(datalist.Any(element => element == item.Id.ToString()))
                     {
                         model.products.Add(item);
                     }
-            });
+            };
             }
             
-
-
-
             return View(model);
         }
     }
