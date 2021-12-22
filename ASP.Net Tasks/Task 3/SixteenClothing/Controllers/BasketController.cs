ca8b049089983a52a29703e27d696cb14dfe69e9
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2021-12-22 20:30:36 +0400

    Some System Changes

---
+++ ASP.Net Tasks/Task 3/SixteenClothing/Controllers/BasketController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SixteenClothing.Data;
+using SixteenClothing.ViewModel;
+using System.Linq;
+
+namespace SixteenClothing.Controllers
+{
+    public class BasketController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public BasketController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            VmBasket model = new VmBasket()
+            {
+                settings = _context.Settings.FirstOrDefault(),
+                products = _context.Products.ToList()
+            };
+            return View(model);
+        }
+    }
+}
