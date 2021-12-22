1593f45457155c157887fa1cbae55fa8f35605b5
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2021-12-22 22:54:37 +0400

    Added Id Field For CartPartial

---
+++ ASP.Net Tasks/Task 3/SixteenClothing/Controllers/CartController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SixteenClothing.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddToCart(int id)
+        {
+
+            string basket = Request.Cookies["basket"];
+
+            if (!string.IsNullOrEmpty(basket))
+            {
+                //List<string> datalist = basket.Split("-").ToList();
+
+                basket = basket + "-" + id;
+
+            }
+            else
+            {
+                basket = id.ToString();
+            }
+
+            Response.Cookies.Append("basket", basket);
+
+
+
+            return RedirectToAction("Index", "Home") ;
+        }
+    }
+}
