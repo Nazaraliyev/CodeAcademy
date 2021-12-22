4234c040f0bbac8ff781caa8322c84dba997ab97
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2021-12-22 23:34:18 +0400

    Import new services for HttpContext

--- ASP.Net Tasks/Task 3/SixteenClothing/Startup.cs
+++ ASP.Net Tasks/Task 3/SixteenClothing/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -26,6 +27,7 @@ namespace SixteenClothing
         {
             services.AddControllersWithViews();
             services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("SixteenClothing")));
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
