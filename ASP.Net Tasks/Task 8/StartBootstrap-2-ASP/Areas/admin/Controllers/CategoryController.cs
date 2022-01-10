c894425d7c0824565004a08224a6a4309a93f740
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2022-01-11 01:09:26 +0400

    Admin Panel Category CRUD is ready

--- ASP.Net Tasks/Task 8/StartBootstrap-2-ASP/Areas/admin/Controllers/CategoryController.cs
+++ ASP.Net Tasks/Task 8/StartBootstrap-2-ASP/Areas/admin/Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using StartBootstrap_2_ASP.Data;
+using StartBootstrap_2_ASP.Models;
 using System.Linq;
 
 namespace StartBootstrap_2_ASP.Areas.admin.Controllers
@@ -16,6 +17,92 @@ namespace StartBootstrap_2_ASP.Areas.admin.Controllers
         public IActionResult Index()
         {
             return View(_context.categories.ToList());
+        }
+
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.categories.Add(model);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+
+
+
+
+        public IActionResult Update(int? Id)
+        {
+            if (Id != null)
+            {
+                if (_context.categories.ToList().Any(c => c.Id == Id))
+                {
+                    return View(_context.categories.Find(Id));
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Update(Category model)
+        {
+            if (ModelState.IsValid && model.Name != null)
+            {
+                _context.categories.Update(model);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+
+
+
+
+
+        public IActionResult Delete(int? Id)
+        {
+            if (Id != null)
+            {
+                if (_context.categories.ToList().Any(c => c.Id == Id))
+                {
+                    _context.categories.Remove(_context.categories.Find(Id));
+                    _context.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            else
+            {
+                return NotFound();
+            }
         }
     }
 }
--- ASP.Net Tasks/Task 8/StartBootstrap-2-ASP/Areas/admin/Controllers/ProductController.cs
+++ ASP.Net Tasks/Task 8/StartBootstrap-2-ASP/Areas/admin/Controllers/ProductController.cs
@@ -1,13 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StartBootstrap_2_ASP.Data;
+using StartBootstrap_2_ASP.Models;
 
 namespace StartBootstrap_2_ASP.Areas.admin.Controllers
 {
     [Area("admin")]
     public class ProductController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
         }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
     }
 }
